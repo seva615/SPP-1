3c1a166bfa17d4408fd2923d863cfb5e896737a8
seva615 <84664893+seva615@users.noreply.github.com>
2021-09-24 02:22:02 +0300

    Delete Spp1/Spp1 directory

--- Spp1/Spp1/ITraceResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using Trace;
-
-namespace TraceResult{
-    public interface ITraceResult{
-        void Save(Stream output, IEnumerable<IThreadNode> traceResult);
-    }
-}
