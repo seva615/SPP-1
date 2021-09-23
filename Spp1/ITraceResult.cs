8a4799f95038b7dd34faf5b8a1fec9dcabb1d733
seva615 <84664893+seva615@users.noreply.github.com>
2021-09-24 02:23:09 +0300

    Add files via upload

---
+++ Spp1/ITraceResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Trace;
+
+namespace TraceResult{
+    public interface ITraceResult{
+        void Save(Stream output, IEnumerable<IThreadNode> traceResult);
+    }
+}
